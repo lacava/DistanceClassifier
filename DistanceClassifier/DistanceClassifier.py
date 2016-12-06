fb6f23825277be91cca5ba1622fb30d8cc622e8f
lacava <williamlacava@gmail.com>
2016-12-06 13:31:27 -0500

    adds support for new version of scikit-learn

--- DistanceClassifier/DistanceClassifier.py
+++ DistanceClassifier/DistanceClassifier.py
@@ -23,7 +23,7 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import accuracy_score
 import pandas as pd
 import numpy as np
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 import argparse
 
 class DistanceClassifier(BaseEstimator):
